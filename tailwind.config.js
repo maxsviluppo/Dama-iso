7bef11d9c082f1107b31d76567d4f0f6cdaac05b
Max1974 <pattuglia-sudoku-8i@icloud.com>
2026-02-09 09:19:02 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,16 @@
 export default {
     content: [
         "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-        "./**/*.{js,ts,jsx,tsx}",
+        "./*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                orbitron: ['Orbitron', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 }
